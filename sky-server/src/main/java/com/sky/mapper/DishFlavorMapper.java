0553f685c999fef07e1bb8f59e86570e3e61b5ab
xzq <shetherwhale@gmail.com>
2024-04-25 19:21:57 +0800

    套餐相关功能

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,13 +1,30 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface DishFlavorMapper {
     void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 根据菜品id删除口味数据
+     * @param dishId
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
+
+    /**
+     * 根据菜品集合批量删除关联的口味数据
+     * @param dishids
+     */
+    void deleteByDishIds(List<Long> dishids);
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishid);
     /**
      * 批量插入口味数据DishMapper.xml
      * @param flavors
